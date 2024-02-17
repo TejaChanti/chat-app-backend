126fb2d1607ff0543d84a311e576ad06d69164bb
TejaChanti <raviteja75@outlook.com>
2024-02-17 21:59:58 +0530

    forth commit

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const server = app.listen(
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://frendzone.netlify.app/",
     // credentials: true,
   },
 });
