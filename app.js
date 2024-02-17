70a714a38914cf592347b46df6bdf8d44f7e20c9
TejaChanti <raviteja75@outlook.com>
2024-02-17 21:32:50 +0530

    second commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import { Server } from 'socket.io';
 
 dotenv.config({ path: '../.env' });
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 const DATABASE_URL = process.env.DATABASE_URL
 
 
